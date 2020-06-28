d0c5dc77c791a17cf43f0f0d3920bf8c0cfd6915
Satyajeet Pal <psatyajeet@gmail.com>
2020-06-28 10:41:24 -0700

    Convert job to run on Heroku scheduler

--- src/jobs.ts
+++ src/jobs.ts
@@ -1,12 +1,13 @@
-import { CronJob } from 'cron';
 import { pullNewWorkoutsAndUpload } from './pullWorkoutsAndUpload';
 
 console.log('Initializing jobs...');
 
-new CronJob(
-  '0 0 * * * *',
-  pullNewWorkoutsAndUpload,
-  '',
-  true,
-  'America/New_York'
-);
+// new CronJob(
+//   '0 0 * * * *',
+//   pullNewWorkoutsAndUpload,
+//   '',
+//   true,
+//   'America/New_York'
+// );
+
+pullNewWorkoutsAndUpload().then(() => console.log('done!'));
